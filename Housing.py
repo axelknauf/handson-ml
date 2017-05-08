d3e8393cd71d772354c1f67bbc452834d4d9b49e
Axel Knauf <axel.knauf@posteo.de>
2017-05-08 07:19:47 +0200

    Cleaned-up version useful for cmdline processing

--- Housing.py
+++ Housing.py
@@ -1,11 +1,11 @@
 import hashlib
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import numpy as np 
 import os 
 import pandas as pd
 import tarfile
 
-from pandas.tools.plotting import scatter_matrix
+#from pandas.tools.plotting import scatter_matrix
 from six.moves import urllib
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.externals import joblib
@@ -15,6 +15,7 @@ from sklearn.model_selection import GridSearchCV, StratifiedShuffleSplit, cross_
 from sklearn.pipeline import FeatureUnion, Pipeline
 from sklearn.preprocessing import Imputer, LabelBinarizer, StandardScaler
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
 
 DOWNLOAD_ROOT="https://raw.githubusercontent.com/ageron/handson-ml/master/"
 HOUSING_PATH="datasets/housing"
@@ -62,14 +63,6 @@ def load_housing_data(housing_path = HOUSING_PATH):
     csv_path = os.path.join(housing_path, "housing.csv")
     return pd.read_csv(csv_path)
 
-def test_set_check(identifier, test_ratio, hash):
-    return hash(np.int64(identifier)).digest()[-1] < 256 * test_ratio
-
-def split_train_test_by_id(data, test_ratio, id_column, hash=hashlib.md5):
-    ids = data[id_column]
-    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio, hash))
-    return data.loc[~in_test_set], data.loc[in_test_set]
-
 def display_scores(scores):
     print("Scores: ", scores)
     print("Mean: ", scores.mean())
@@ -77,43 +70,46 @@ def display_scores(scores):
 
 # ---- PROCESSING ----
 
-# ==== 1) fetch and extract source data
+print("Fetching and extracting source data")
 fetch_housing_data()
 housing = load_housing_data()
 
-# ==== 2) create test and training sets using derived column
-# 2.1) Create column for income categories
+print("create test and training sets using derived column")
+print("Create column for income categories")
 housing["income_cat"] = np.ceil(housing["median_income"] / 1.5)
 housing["income_cat"].where(housing["income_cat"] < 5, 5.0, inplace=True)
 
-# 2.2) Create test and trainings sets using this new column
+print("Create test and trainings sets using new column")
 split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
 for train_index, test_index in split.split(housing, housing["income_cat"]):
     strat_train_set = housing.loc[train_index]
     strat_test_set = housing.loc[test_index]
 
-# 2.3) remove temporary column again from sets 
+print("Remove temporary column again from sets")
 for set in (strat_test_set, strat_train_set):
     set.drop(["income_cat"], axis=1, inplace=True)
 
-# 2.4) Make sure we are using only the training set for our models
+print("Make sure we are using only the training set for our models")
 housing = strat_train_set.copy()
 
-# ==== 3) add new derived columns on training set "housing"
+print("add new derived columns on training set 'housing'")
 housing["rooms_per_household"] = housing["total_rooms"] / housing["households"]
 housing["bedrooms_per_room"] = housing["total_bedrooms"] / housing["total_rooms"]
 housing["population_per_household"] = housing["population"] / housing["households"]
 
-# FIXME does this make sense / work at all?
+# drop() creates a copy and does not affect the source set, so this works
 housing = strat_train_set.drop("median_house_value", axis=1)
 housing_labels = strat_train_set["median_house_value"].copy()
 
-# ==== 4) Set up processing pipeline
+print("Set up processing pipeline")
 # Recap:
 # - housing        -> stratified training set
 # - housing_num    -> only numerical attributes / features
 # - housing_labels -> real result values, used to calculate error for models
 housing_num = housing.drop("ocean_proximity", axis=1)
+
+# Custom column indexes, required for CombinedAttributesAdder etc.
+rooms_ix, bedrooms_ix, population_ix, household_ix = 3, 4, 5, 6
 
 num_attribs = list(housing_num)
 cat_attribs = ["ocean_proximity"]
@@ -135,62 +131,65 @@ full_pipeline = FeatureUnion(transformer_list=[
     ("cat_pipeline", cat_pipeline),
 ])
 
+print("Fit & transform pipeline to data")
 housing_prepared = full_pipeline.fit_transform(housing)
 
-# ==== 9) Train different models and evaluate error
+# # ==== 9) Train different models and evaluate error
+# 
+# # === 9.1) Linear Regression
+# lin_reg = LinearRegression()
+# lin_reg.fit(housing_prepared, housing_labels)
+# 
+# some_data = housing.iloc[:5]
+# some_labels = housing_labels.iloc[:5]
+# some_data_prepared = full_pipeline.transform(some_data)  # NO fit()!
+# print("Predictions:\t", lin_reg.predict(some_data_prepared))
+# print("Labels:\t\t", list(some_labels))
+# 
+# lin_scores = cross_val_score(lin_reg, housing_prepared, housing_labels, scoring="neg_mean_squared_error", cv=10)
+# lin_rmse_scores = np.sqrt(-lin_scores)
+# 
+# print("Linear regression: ")
+# display_scores(lin_rmse_scores)
+# 
+# # === 9.2) Decision Tree Regressor
+# tree_reg = DecisionTreeRegressor()
+# tree_reg.fit(housing_prepared, housing_labels)
+# 
+# housing_predictions = tree_reg.predict(housing_prepared)
+# tree_mse = mean_squared_error(housing_labels, housing_predictions)
+# tree_mrse = np.sqrt(tree_mse)
+# print("MRSE (tree): ", tree_mrse, " (obviously overfitting)")
+# 
+# tree_scores = cross_val_score(tree_reg, housing_prepared, housing_labels, scoring="neg_mean_squared_error", cv=10)
+# tree_rmse_scores = np.sqrt(-tree_scores)
+# 
+# print("Decision tree: ")
+# display_scores(tree_rmse_scores)
+# 
+# # === 9.3) Random Forest Regressor
+# forest_reg = RandomForestRegressor()
+# forest_reg.fit(housing_prepared, housing_labels)
+# 
+# housing_predictions = forest_reg.predict(housing_prepared)
+# forest_mse = mean_squared_error(housing_labels, housing_predictions)
+# forest_mrse = np.sqrt(forest_mse)
+# print("MRSE (forest): ", forest_mrse)
+# 
+# forest_scores = cross_val_score(forest_reg, housing_prepared, housing_labels, scoring="neg_mean_squared_error", cv=10)
+# forest_rmse_scores = np.sqrt(-forest_scores)
+# 
+# print("Forest scores: ")
+# display_scores(forest_rmse_scores)
 
-# === 9.1) Linear Regression
-lin_reg = LinearRegression()
-lin_reg.fit(housing_prepared, housing_labels)
 
-some_data = housing.iloc[:5]
-some_labels = housing_labels.iloc[:5]
-some_data_prepared = full_pipeline.transform(some_data)  # NO fit()!
-print("Predictions:\t", lin_reg.predict(some_data_prepared))
-print("Labels:\t\t", list(some_labels))
-
-lin_scores = cross_val_score(lin_reg, housing_prepared, housing_labels, scoring="neg_mean_squared_error", cv=10)
-lin_rmse_scores = np.sqrt(-lin_scores)
-
-print("Linear regression: ")
-display_scores(lin_rmse_scores)
-
-# === 9.2) Decision Tree Regressor
-tree_reg = DecisionTreeRegressor()
-tree_reg.fit(housing_prepared, housing_labels)
-
-housing_predictions = tree_reg.predict(housing_prepared)
-tree_mse = mean_squared_error(housing_labels, housing_predictions)
-tree_mrse = np.sqrt(tree_mse)
-print("MRSE (tree): ", tree_mrse, " (obviously overfitting)")
-
-tree_scores = cross_val_score(tree_reg, housing_prepared, housing_labels, scoring="neg_mean_squared_error", cv=10)
-tree_rmse_scores = np.sqrt(-tree_scores)
-
-print("Decision tree: ")
-display_scores(tree_rmse_scores)
-
-# === 9.3) Random Forest Regressor
-forest_reg = RandomForestRegressor()
-forest_reg.fit(housing_prepared, housing_labels)
-
-housing_predictions = forest_reg.predict(housing_prepared)
-forest_mse = mean_squared_error(housing_labels, housing_predictions)
-forest_mrse = np.sqrt(forest_mse)
-print("MRSE (forest): ", forest_mrse)
-
-forest_scores = cross_val_score(forest_reg, housing_prepared, housing_labels, scoring="neg_mean_squared_error", cv=10)
-forest_rmse_scores = np.sqrt(-forest_scores)
-
-print("Forest scores: ")
-display_scores(forest_rmse_scores)
-
-
-# ==== 10) Use grid search to find best parameters
+print("Using grid search to find best parameters for RandomForestRegressor")
 param_grid = [
     { 'n_estimators' : [3, 10, 30], 'max_features' : [2, 4, 6, 8] },
     { 'bootstrap' : [False], 'n_estimators' : [3, 10], 'max_features' : [2, 3, 4] }
 ]
+
+print("Parameters are ", param_grid)
 
 forest_reg = RandomForestRegressor()
 
@@ -200,19 +199,21 @@ grid_search.fit(housing_prepared, housing_labels)
 print("Grid search, best params:\n--------------\n", grid_search.best_params_)
 print("Grid search, best estimator:\n--------------\n", grid_search.best_estimator_)
 print("Grid search, cross-evaluation scores:\n--------------\n")
+
+print("Results for all")
 cvres = grid_search.cv_results_
 for mean_score, params in zip(cvres["mean_test_score"], cvres["params"]):
     print(np.sqrt(-mean_score), params)
 
 
-feature_importances = grid_search.best_estimator_.feature_importances_
-extra_attribs = ["rooms_per_hhold", "pop_per_hhold", "bedrooms_per_room"]
-cat_one_hot_attribs = list(encoder.classes_)
-attributes = num_attribs + extra_attribs + cat_one_hot_attribs
-sorted(zip(feature_importances, attributes), reverse=True)
+# feature_importances = grid_search.best_estimator_.feature_importances_
+# extra_attribs = ["rooms_per_hhold", "pop_per_hhold", "bedrooms_per_room"]
+# cat_one_hot_attribs = list(encoder.classes_)
+# attributes = num_attribs + extra_attribs + cat_one_hot_attribs
+# sorted(zip(feature_importances, attributes), reverse=True)
 
 
-# ==== 11) Use best parameters / model to verify against the test set
+print("Use best parameters / model to verify against the test set")
 final_model = grid_search.best_estimator_
 
 X_test = strat_test_set.drop("median_house_value", axis=1)
